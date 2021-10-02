e23d47afab5a6d72a9e9f1e5d8ec8ede64bedcee
koki <s17161278@st.daito.ac.jp>
2021-10-02 13:57:18 +0900

    topページの作成
    BootstrapVueの導入(CDN版)

--- wordbookproject/vocabulary/urls.py
+++ wordbookproject/vocabulary/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from .views import return_words
 
 
+"""TODO
+    path('languages/',  静的テンプレートで)
+    path('<language(path_parametar)>/')
+    ex: python/noun/, java/adverb/
+    path('<language(path_parameter)>/<pos(path_parameter)>/')
+"""
 urlpatterns = [
-    path('words/', return_words, name='words')
+    path('words/', return_words, name='words'),
 ]
