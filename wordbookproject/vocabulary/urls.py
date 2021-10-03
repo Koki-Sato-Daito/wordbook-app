23a6edf78e2522bd2c5610c7e9fe243fe71958a0
koki <s17161278@st.daito.ac.jp>
2021-10-03 15:47:57 +0900

    言語選択画面の追加 DTLとVue.jsで重複したマスタッシュの改善(vue側を${}に変更) ブラウザのVue Devtoolでデバッグできるように設定

--- wordbookproject/vocabulary/urls.py
+++ wordbookproject/vocabulary/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 from .views import return_words
 
 
 """TODO
-    path('languages/',  静的テンプレートで)
-    path('<language(path_parametar)>/')
     ex: python/noun/, java/adverb/
     path('<language(path_parameter)>/<pos(path_parameter)>/')
 """
 urlpatterns = [
     path('words/', return_words, name='words'),
+    path('languages/', TemplateView.as_view(
+        template_name='vocabulary/languages.html'), name='languages')
 ]
