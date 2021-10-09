5c76b8efde3d0fc3e7cfeaf076c0b5195d8067f6
koki <s17161278@st.daito.ac.jp>
2021-10-09 23:07:35 +0900

    Vocabularyモデルをadminに登録 wordbook.htmlを追加し、axiosでデータをfetchする部分を実装

--- wordbookproject/vocabulary/admin.py
+++ wordbookproject/vocabulary/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from vocabulary.models import Vocabulary
+
+
+class VocabularyAdmin(admin.ModelAdmin):
+    filedsets = [
+        (None, {'fields': ['wordname', 'meaning', 'pos']}),
+        ('Language', {'fields': ['python_freq']},)
+    ]
+
+    list_display = ('wordname', 'pos')
+    search_fields = ['wordname', 'meaning']
+
+admin.site.register(Vocabulary, VocabularyAdmin)
