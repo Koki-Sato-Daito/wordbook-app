6d8b140670c5f812dcbd3971c66a4730b6e04267
koki <s17161278@st.daito.ac.jp>
2022-02-27 20:53:33 +0900

    開発環境にdockerを追加

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
