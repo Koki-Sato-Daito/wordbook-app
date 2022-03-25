9fc45837eed2b6655b03b24ba090f2ec76f22060
koki <s17161278@st.daito.ac.jp>
2022-03-25 22:54:38 +0900

    APIサーバのテストviewを削除

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='top.html'), name='top'),
     path('api/v1/', include('apiv1.urls')),
 ]
 
