878c139f708b1c37a3ead9475b6e55daf16ed995
koki <s17161278@st.daito.ac.jp>
2022-05-12 23:48:42 +0900

    nuxtClientInitの例外処理を追加

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,14 +1,18 @@
 const actions = {
     nuxtClientInit({ commit }, context) {
-        const keys = JSON.parse(localStorage.getItem("wordbook"));
-        const payload = {}
-        payload.auth_token = keys.authentication.authToken;
-        
-        payload.user = {}
-        payload.user.id = keys.authentication.userData.id;
-        payload.user.username  = keys.authentication.userData.username;
-        payload.user.email = keys.authentication.userData.email;
-        commit('authentication/setAuthData', payload);
+        try{
+            const keys = JSON.parse(localStorage.getItem("wordbook"));
+            const payload = {}
+            payload.auth_token = keys.authentication.authToken;
+            
+            payload.user = {}
+            payload.user.id = keys.authentication.userData.id;
+            payload.user.username  = keys.authentication.userData.username;
+            payload.user.email = keys.authentication.userData.email;
+            commit('authentication/setAuthData', payload);
+        }catch(error) {
+            console.log(error);
+        }
       }
 }
 
